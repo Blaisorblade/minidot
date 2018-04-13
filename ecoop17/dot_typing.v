a6e4aec30b8d1220b9ab6dfa0e5b26c8d8e505e3
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2018-04-13 16:21:08 +0200

    Fix dot_typing after rename

--- ecoop17/dot_typing.v
+++ ecoop17/dot_typing.v
@@ -8,10 +8,10 @@ not required here.
 *)
 Inductive stp: tenv -> ty -> ty -> Prop :=
 | stp_top: forall GH T1,
-    closed 0 (length GH) T1 ->
+    closed_ty 0 (length GH) T1 ->
     stp GH T1 TTop
 | stp_bot: forall GH T2,
-    closed 0 (length GH) T2 ->
+    closed_ty 0 (length GH) T2 ->
     stp GH TBot T2
 | stp_mem: forall GH S1 U1 S2 U2,
     stp GH U1 U2 ->
@@ -20,12 +20,12 @@ Inductive stp: tenv -> ty -> ty -> Prop :=
 
 | stp_sel1: forall GH TX T2 x,
     indexr x GH = Some TX ->
-    closed 0 (length GH) TX -> (* XXX unclear if length GH is correct, the existing code is inconsistent. *)
+    closed_ty 0 (length GH) TX -> (* XXX unclear if length GH is correct, the existing code is inconsistent. *)
     stp GH TX (TMem TBot T2) ->
     stp GH (TSel (varF x)) T2
 | stp_sel2: forall GH TX T1 x,
     indexr x GH = Some TX ->
-    closed 0 (length GH) TX ->
+    closed_ty 0 (length GH) TX ->
     stp GH TX (TMem T1 TTop) ->
     stp GH T1 (TSel (varF x)) 
 | stp_selx: forall GH TX x,
@@ -37,18 +37,18 @@ Inductive stp: tenv -> ty -> ty -> Prop :=
     stp (T1'::GH) T1' T2' ->
     T1' = (open (varF (length GH)) T1) ->
     T2' = (open (varF (length GH)) T2) ->
-    closed 1 (length GH) T1 ->
-    closed 1 (length GH) T2 ->
+    closed_ty 1 (length GH) T1 ->
+    closed_ty 1 (length GH) T2 ->
     stp GH (TBind T1) (TBind T2)
 
 | stp_and11: forall GH T1 T2 T,
     stp GH T1 T ->
-    closed 0 (length GH) T2 ->
+    closed_ty 0 (length GH) T2 ->
     stp GH (TAnd T1 T2) T
 
 | stp_and12: forall GH T1 T2 T,
     stp GH T2 T ->
-    closed 0 (length GH) T1 ->
+    closed_ty 0 (length GH) T1 ->
     stp GH (TAnd T1 T2) T
 
 | stp_and2: forall GH T1 T2 T,
@@ -60,8 +60,8 @@ Inductive stp: tenv -> ty -> ty -> Prop :=
 | stp_all: forall GH T1 T2 T3 T4 x,
     stp GH T3 T1 ->
     x = length GH ->
-    closed 1 (length GH) T2 ->
-    closed 1 (length GH) T4 ->
+    closed_ty 1 (length GH) T2 ->
+    closed_ty 1 (length GH) T4 ->
     stp (T3::GH) (open (varF x) T2) (open (varF x) T4) ->
     stp GH (TAll T1 T2) (TAll T3 T4)
 | stp_trans: forall GH T1 T2 T3,
@@ -81,14 +81,14 @@ Inductive has_type : tenv -> tm -> ty -> Prop :=
            (* has_type G1 (tvar x) T1' -> *)
            indexr x GH = Some (open (varF x) T1) ->            
            T1' = (open (varF x) T1) ->
-           closed 1 (length GH) T1 ->
+           closed_ty 1 (length GH) T1 ->
            has_type GH (tvar x) (TBind T1) 
 (* unpack a recursive type: unpack(x:{z=>T^z}) { x:T^x => ... }  *)                    
 | t_unpack: forall env x y T1 T1' T2,
            has_type env x (TBind T1) ->
            T1' = (open (varF (length env)) T1) ->
            has_type (T1'::env) y T2 ->
-           closed 0 (length env) T2 ->
+           closed_ty 0 (length env) T2 ->
            has_type env (tunpack x y) T2
 
 (* intersection typing *)
@@ -99,23 +99,23 @@ Inductive has_type : tenv -> tm -> ty -> Prop :=
 
                
 | t_typ: forall env T1,
-           closed 0 (length env) T1 ->
+           closed_ty 0 (length env) T1 ->
            has_type env (ttyp T1) (TMem T1 T1)
                
 | t_app: forall env f x T1 T2,
            has_type env f (TAll T1 T2) ->
            has_type env x T1 ->
-           closed 0 (length env) T2 ->
+           closed_ty 0 (length env) T2 ->
            has_type env (tapp f x) T2
 | t_dapp:forall env f x T1 T2 T,
            has_type env f (TAll T1 T2) ->
            has_type env (tvar x) T1 ->
            T = open (varF x) T2 ->
-           closed 0 (length env) T ->
+           closed_ty 0 (length env) T ->
            has_type env (tapp f (tvar x)) T
 | t_abs: forall env y T1 T2,
            has_type (T1::env) y (open (varF (length env)) T2) ->
-           closed 0 (length env) (TAll T1 T2) ->
+           closed_ty 0 (length env) (TAll T1 T2) ->
            has_type env (tabs T1 y) (TAll T1 T2)
 | t_sub: forall env e T1 T2,
            has_type env e T1 ->
