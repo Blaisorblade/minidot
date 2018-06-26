388966f4ac8de66a89ae7f64bcbf29233940f583
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2018-06-26 04:09:14 +0200

    Start proving subtyping lemmas

--- ecoop17/dot_storeless_wfrel.v
+++ ecoop17/dot_storeless_wfrel.v
@@ -387,6 +387,41 @@ Proof.
 Qed.
 Hint Resolve vl_subtype_some_to_subtype_some.
 
+Lemma and_stp1 : forall T1 T2 n v, vtp (TAnd T1 T2) n v -> vtp T1 n v.
+Proof. unfold vtp; intros; simp val_type in *; tauto. Qed.
+
+Hint Unfold wf sem_type_some sem_subtype_some sem_vl_subtype_some.
+
+Ltac to_vl_stp L :=
+  unfold sem_vl_subtype_some, vtpEnvSomeCore;
+    intuition eauto; ev;
+      simpl in *; repeat better_case_match; try congruence; injections_some;
+        eauto using L.
+
+Lemma sem_vl_and_stp1 : forall G T1 T2, wf G T1 -> wf G T2 -> sem_vl_subtype_some G (TAnd T1 T2) T1.
+Proof. to_vl_stp and_stp1. Qed.
+
+Lemma sem_and_stp1 : forall G T1 T2, wf G T1 -> wf G T2 -> sem_subtype_some G (TAnd T1 T2) T1.
+Proof. eauto using vl_subtype_some_to_subtype_some, sem_vl_and_stp1. Qed.
+
+Lemma and_stp2 : forall T1 T2 n v, vtp (TAnd T1 T2) n v -> vtp T2 n v.
+Proof. unfold vtp; intros; simp val_type in *; tauto. Qed.
+
+Lemma sem_vl_and_stp2 : forall G T1 T2, wf G T1 -> wf G T2 -> sem_vl_subtype_some G (TAnd T1 T2) T2.
+Proof. to_vl_stp and_stp2. Qed.
+
+Lemma sem_and_stp2 : forall G T1 T2, wf G T1 -> wf G T2 -> sem_subtype_some G (TAnd T1 T2) T2.
+Proof. eauto using vl_subtype_some_to_subtype_some, sem_vl_and_stp2. Qed.
+
+Lemma stp_and' : forall T1 T2 n v, vtp T1 n v -> vtp T2 n v -> vtp (TAnd T1 T2) n v.
+Proof. unfold vtp; intros; simp val_type in *; tauto. Qed.
+
+Lemma stp_and : forall S T1 T2 n v,
+    (vtp S n v -> vtp T1 n v) ->
+    (vtp S n v -> vtp T2 n v) ->
+    vtp S n v -> vtp (TAnd T1 T2) n v.
+Proof. unfold vtp; intros; simp val_type in *; tauto. Qed.
+
 (* Lemma tm_closed_irrelevance1: forall i1 i2 k1 k2 t *)
 (*                                (H1 : tm_closed i1 k1 t) (H2 : tm_closed i2 k2 t) *)
 (*                                (Hi : i1 ~= i2) (Hk: k1 ~= k2), H1 ~= H2. *)
