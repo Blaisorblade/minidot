69d057934375c26c679f0ecf21b9698e86c7120d
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2018-06-29 12:14:59 +0200

    dot_storeless_wfrel: Remove _some from names

--- ecoop17/dot_storeless_wfrel.v
+++ ecoop17/dot_storeless_wfrel.v
@@ -43,72 +43,72 @@ Lemma etpEnv_closed: forall T k v l env,
 Proof. unfold etpEnv; program_simpl. Qed.
 Hint Resolve etpEnv_closed.
 
-Definition sem_type_some (G : tenv) (T : ty) (e: tm) :=
+Definition sem_type (G : tenv) (T : ty) (e: tm) :=
   wf G T /\ tm_closed 0 (length G) e /\
       forall k env (Henv: R_env k env G), etpEnvCore T k e 0 env.
 
-Lemma sem_type_some_closed : forall G T e,
-    sem_type_some G T e -> wf G T.
-Proof. unfold sem_type_some; program_simpl. Qed.
+Lemma sem_type_closed : forall G T e,
+    sem_type G T e -> wf G T.
+Proof. unfold sem_type; program_simpl. Qed.
 
-Definition sem_subtype_some (G : tenv) (T1 T2: ty) :=
+Definition sem_subtype (G : tenv) (T1 T2: ty) :=
   wf G T1 /\ wf G T2 /\
       forall e (HwfE : tm_closed 0 (length G) e),
       forall k env (Henv : R_env k env G),
         etpEnvCore T1 k e 0 env -> etpEnvCore T2 k e 0 env.
 
-Definition sem_vl_subtype_some (G : tenv) (T1 T2: ty) :=
+Definition sem_vl_subtype (G : tenv) (T1 T2: ty) :=
   wf G T1 /\ wf G T2 /\
       forall v (HwfV : tm_closed 0 0 v),
       forall k env (Henv : R_env k env G),
         vtpEnvCore T1 k v env -> vtpEnvCore T2 k v env.
 
-Lemma sem_subtype_some_closed1 : forall G T1 T2,
-    sem_subtype_some G T1 T2 -> wf G T1.
-Proof. unfold sem_subtype_some; program_simpl. Qed.
+Lemma sem_subtype_closed1 : forall G T1 T2,
+    sem_subtype G T1 T2 -> wf G T1.
+Proof. unfold sem_subtype; program_simpl. Qed.
 
-Lemma sem_subtype_some_closed2 : forall G T1 T2,
-    sem_subtype_some G T1 T2 -> wf G T2.
-Proof. unfold sem_subtype_some; program_simpl. Qed.
+Lemma sem_subtype_closed2 : forall G T1 T2,
+    sem_subtype G T1 T2 -> wf G T2.
+Proof. unfold sem_subtype; program_simpl. Qed.
 
-Lemma sem_vl_subtype_some_closed1 : forall G T1 T2,
-    sem_vl_subtype_some G T1 T2 -> wf G T1.
-Proof. unfold sem_vl_subtype_some; program_simpl. Qed.
+Lemma sem_vl_subtype_closed1 : forall G T1 T2,
+    sem_vl_subtype G T1 T2 -> wf G T1.
+Proof. unfold sem_vl_subtype; program_simpl. Qed.
 
-Lemma sem_vl_subtype_some_closed2 : forall G T1 T2,
-    sem_vl_subtype_some G T1 T2 -> wf G T2.
-Proof. unfold sem_vl_subtype_some; program_simpl. Qed.
+Lemma sem_vl_subtype_closed2 : forall G T1 T2,
+    sem_vl_subtype G T1 T2 -> wf G T2.
+Proof. unfold sem_vl_subtype; program_simpl. Qed.
 
-Hint Resolve sem_type_some_closed
-     sem_subtype_some_closed1
-     sem_subtype_some_closed2
-     sem_vl_subtype_some_closed1
-     sem_vl_subtype_some_closed2.
+Hint Resolve sem_type_closed
+     sem_subtype_closed1
+     sem_subtype_closed2
+     sem_vl_subtype_closed1
+     sem_vl_subtype_closed2.
 
-Lemma vl_subtype_some_to_subtype_some : forall G T1 T2
-    (Hsub: sem_vl_subtype_some G T1 T2), sem_subtype_some G T1 T2.
-Proof. unfold sem_subtype_some, sem_vl_subtype_some, etpEnvCore; intuition eauto 10. Qed.
-Hint Resolve vl_subtype_some_to_subtype_some.
+Lemma vl_subtype_to_subtype : forall G T1 T2
+    (Hsub: sem_vl_subtype G T1 T2), sem_subtype G T1 T2.
+Proof. unfold sem_subtype, sem_vl_subtype, etpEnvCore; intuition eauto 10. Qed.
+Hint Resolve vl_subtype_to_subtype.
 
-Hint Unfold wf sem_type_some sem_subtype_some sem_vl_subtype_some.
+Hint Unfold wf sem_type sem_subtype sem_vl_subtype.
 
 Ltac to_vl_stp L :=
-  unfold sem_vl_subtype_some, vtpEnvCore;
+  unfold sem_vl_subtype, vtpEnvCore;
     intuition eauto; ev;
       simpl in *; repeat better_case_match; try congruence; injectHyps;
         eauto using L.
 
-Lemma sem_vl_and_stp1 : forall G T1 T2, wf G T1 -> wf G T2 -> sem_vl_subtype_some G (TAnd T1 T2) T1.
+Lemma sem_vl_and_stp1 : forall G T1 T2, wf G T1 -> wf G T2 -> sem_vl_subtype G (TAnd T1 T2) T1.
 Proof. to_vl_stp and_stp1. Qed.
 
-Lemma sem_and_stp1 : forall G T1 T2, wf G T1 -> wf G T2 -> sem_subtype_some G (TAnd T1 T2) T1.
-Proof. eauto using vl_subtype_some_to_subtype_some, sem_vl_and_stp1. Qed.
+Lemma sem_and_stp1 : forall G T1 T2, wf G T1 -> wf G T2 -> sem_subtype G (TAnd T1 T2) T1.
+Proof. eauto using vl_subtype_to_subtype, sem_vl_and_stp1. Qed.
 
-Lemma sem_vl_and_stp2 : forall G T1 T2, wf G T1 -> wf G T2 -> sem_vl_subtype_some G (TAnd T1 T2) T2.
+Lemma sem_vl_and_stp2 : forall G T1 T2, wf G T1 -> wf G T2 -> sem_vl_subtype G (TAnd T1 T2) T2.
 Proof. to_vl_stp and_stp2. Qed.
 
-Lemma sem_and_stp2 : forall G T1 T2, wf G T1 -> wf G T2 -> sem_subtype_some G (TAnd T1 T2) T2.
-Proof. eauto using vl_subtype_some_to_subtype_some, sem_vl_and_stp2. Qed.
+Lemma sem_and_stp2 : forall G T1 T2, wf G T1 -> wf G T2 -> sem_subtype G (TAnd T1 T2) T2.
+Proof. eauto using vl_subtype_to_subtype, sem_vl_and_stp2. Qed.
 
 Hint Resolve Nat.le_max_l Nat.le_max_r.
 Lemma some_max_lemma: forall j k,
@@ -177,9 +177,9 @@ Lemma vtpEnvCoreToEval: forall T k v env, vtpEnvCore T k v env -> tm_closed 0 0 
 Qed.
 
 Lemma subtype_to_vl_subtype : forall G T1 T2,
-    sem_subtype_some G T1 T2 -> sem_vl_subtype_some G T1 T2.
+    sem_subtype G T1 T2 -> sem_vl_subtype G T1 T2.
 Proof.
-  unfold sem_subtype_some, sem_vl_subtype_some, etpEnvCore; intros * (? & ? & Hsub);
+  unfold sem_subtype, sem_vl_subtype, etpEnvCore; intros * (? & ? & Hsub);
     split_conj; eauto;
       intros;
     eapply Hsub with (e := v); omega || eauto using vtpEnvCoreToEval; intros.
