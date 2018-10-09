eec205a6e9b21b179b46c0b6053952b70684422b
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2018-10-09 20:22:43 +0200

    stlc.v: split in more files

---
+++ ecoop17/stlc_norm.v
@@ -0,0 +1,129 @@
+(** Prove STLC normalization: all well-typed terms evaluate to a value. *)
+Require Import stlc.
+Require Import Equations.Equations.
+
+Definition expr_sem (A : vl_prop) (t : tm) (env: venv): Prop :=
+  exists v j,
+    tevalSn env t v j /\ A v.
+
+(* Non-step-indexed reducibility *)
+Equations vtp (T: ty) (v : vl) : Prop :=
+  vtp T t by rec T tsize_rel :=
+  vtp TNat (vnat n) := True;
+  vtp (TFun T1 T2) (vabs env body) := forall v, vtp T1 v -> expr_sem (fun v => vtp T2 v) body (v :: env);
+  vtp _ _ := False.
+Solve All Obligations with program_simpl.
+
+Example ex0 : vtp (TFun TNat TNat) (vabs [] (tvar 0)).
+Proof.
+  simp vtp; unfold expr_sem; unfoldTeval; intros.
+  do 3 eexists; try exists 0; intros; try step_eval; eauto.
+Qed.
+
+(* Require Import Setoid. *)
+(* Stolen from https://github.com/coq/coq/issues/3814, and dangerous, but enable setoid_rewrite using equalities on the goal. *)
+(* Instance subrelation_eq_impl : subrelation eq impl. congruence. Qed. *)
+(* Instance subrelation_eq_flip_impl : subrelation eq (flip impl). congruence. Qed. *)
+
+Include Envs.
+
+Lemma vtp_etp: forall e v j nm T env,
+    tevalSnm env e v j nm -> vtp T v -> etp T e env.
+Proof. unfold etp, expr_sem in *; intros; unfoldTeval; ev; eauto. Qed.
+Hint Resolve vtp_etp.
+
+Lemma etp_vtp: forall e v j nm T env,
+    tevalSnm env e v j nm -> etp T e env -> vtp T v.
+Proof. unfold etp, expr_sem; unfold2tevalSnmOpt; intros; ev; eval_det; eauto. Qed.
+(* Unused *)
+(* Hint Resolve etp_vtp. *)
+
+Lemma fund_t_abs: forall G T1 T2 t,
+    sem_type (T1 :: G) T2 t ->
+    sem_type G (TFun T1 T2) (tabs t).
+Proof.
+  unfold sem_type; simpl; intros; eapply vtp_etp with (nm := 0).
+  - unfoldTeval; intros; step_eval; trivial.
+  - unfold etp in *; simp vtp; eauto.
+Qed.
+
+Lemma fund_t_nat: forall G n,
+    sem_type G TNat (tnat n).
+Proof.
+  unfold sem_type; intros; eapply vtp_etp with (nm := 0).
+  - unfoldTeval; intros; step_eval; trivial.
+  - unfold etp in *; simp vtp; eauto.
+Qed.
+
+Lemma fund_t_var: forall G x T, indexr x G = Some T -> sem_type G T (tvar x).
+Proof.
+  unfold sem_type, etp, expr_sem; intros.
+  pose proof (teval_var env x).
+  unfold2tevalSnmOpt; ev; subst.
+  edestruct R_env_to_indexr_success as [? Hrew]; eauto.
+  rewrite Hrew in *.
+  repeat eexists; eauto.
+Qed.
+
+Lemma fund_t_app: forall G T1 T2 t1 t2, sem_type G (TFun T1 T2) t1 -> sem_type G T1 t2 -> sem_type G T2 (tapp t1 t2).
+Proof.
+  unfold sem_type, etp, expr_sem; unfoldTeval.
+  intros * Hfun Harg * Henv.
+  specialize (Hfun _ Henv) as (v1 & j1 & (nm1 & Hev1) & Hvtp1);
+  specialize (Harg _ Henv) as (v2 & j2 & (nm2 & Hev2) & Hvtp2).
+
+  (* v1 is a function by semantic well-typing. *)
+  destruct v1; simp vtp in *; try contradiction.
+  unfold expr_sem in *; unfoldTeval.
+  edestruct Hvtp1 as (vR & jR & (nmR & HevR) & HvtpR); eauto; clear Hvtp1.
+  remember (max nm1 nm2) as nmA.
+  remember (max nmA nmR) as nm.
+  assert (nm >= nm1 /\ nm >= nm2 /\ nm >= nmR) as (? & ? & ?). {
+    assert (nmA >= nm1 /\ nmA >= nm2) as (? & ?) by (subst; eauto using max_bigger_both);
+      assert (nm >= nmA /\ nm >= nmR) as (? & ?) by (subst; eauto using max_bigger_both);
+      intuition eauto.
+  }
+
+  (* Align all assumtions to use nm. *)
+  repeat match goal with
+  | H1 : forall n, n > ?nm1 -> tevalS ?t1 n ?env = Some ?r1 |- _ =>
+    assert (tevalSnmOpt env t1 (fst r1) (snd r1) nm) by (simpl; unfoldTeval; eauto);
+    clear H1
+  end; unfoldTeval; simpl in *.
+  remember (S nm) as nm'.
+
+  do 2 eexists; split_conj; try exists (S nm'); eauto.
+
+  intros; step_eval; unfold logStep in *;
+  repeat match goal with
+  | H : forall n, ?nm <= n -> _ |- _ => lets ?: H n0 __; eauto; clear H end.
+  now repeat better_case_match_ex.
+Qed.
+
+(** Fundamental property.
+    Proved by induction on typing derivations. *)
+Theorem fundamental: forall G t T, has_type G t T -> sem_type G T t.
+Proof. intros * Htp; induction Htp; eauto using fund_t_var, fund_t_nat, fund_t_abs, fund_t_app. Qed.
+
+(** Strong normalization: evaluation of a well-typed program terminates with a
+    result of the right type. Proved as a corollary of the [fundamental] property.
+  *)
+Theorem normalize: forall G t T env, has_type G t T ->
+    R_env env G ->
+    exists v j,
+      tevalSn env t v j /\ vtp T v.
+Proof. intros; edestruct fundamental; ev; eauto. Qed.
+
+(** Type soundness: If evaluation of a well-typed program terminates, the result
+    is not a runtime error. Proved as a corollary of [normalize]. *)
+Theorem sound: forall G t T env optV j, has_type G t T ->
+    R_env env G ->
+    tevalSnOpt env t optV j ->
+    exists v, optV = Some v /\ vtp T v.
+Proof.
+  (** [t] normalizes. *)
+  intros; edestruct normalize; ev; eauto.
+  (** Since evaluation is deterministic, the given evaluation has the same
+      result, hence is sound. *)
+  unfold2tevalSnmOpt; eval_det; eauto.
+Qed.
---
+++ ecoop17/stlc_sound.v
@@ -0,0 +1,121 @@
+(** Prove STLC type soundness: no well-typed terms causes a runtime type error. *)
+Require Import stlc.
+Require Import Equations.Equations.
+
+(* Maybe make both normal definitions, or at least Program Definitions? Let's limit equations weird rules? *)
+(* Only expr_sem changes in definitions between here and normalization. *)
+Definition expr_sem (A : vl_prop) (t : tm) (env: venv): Prop :=
+  forall optV j,
+    tevalSnOpt env t optV j -> exists v, optV = Some v /\ A v.
+
+(* Non-step-indexed unary logical relation. *)
+Equations vtp (T: ty) (v : vl) : Prop :=
+  vtp T t by rec T tsize_rel :=
+  vtp TNat (vnat n) := True;
+  vtp (TFun T1 T2) (vabs env body) := forall v, vtp T1 v -> expr_sem (fun v => vtp T2 v) body (v :: env);
+  vtp _ _ := False.
+Solve All Obligations with program_simpl.
+
+Example ex0 : vtp (TFun TNat TNat) (vabs [] (tvar 0)).
+Proof.
+  simp vtp; unfold expr_sem; intros.
+  unfoldTeval; n_is_succ_hp; eauto.
+Qed.
+
+Include Envs.
+
+Lemma vtp_etp: forall e v j nm T env,
+    tevalSnm env e v j nm -> vtp T v -> etp T e env.
+Proof. unfold etp, expr_sem in *; intros; unfoldTeval; ev; eauto. Qed.
+Hint Resolve vtp_etp.
+
+Lemma etp_vtp: forall e v j nm T env,
+    tevalSnm env e v j nm -> etp T e env -> vtp T v.
+Proof. unfold etp, expr_sem; unfold2tevalSnmOpt; intros * ? H; edestruct H; ev; eauto. Qed.
+(* Unused *)
+(* Hint Resolve etp_vtp. *)
+
+Lemma fund_t_abs: forall G T1 T2 t,
+  sem_type (T1 :: G) T2 t ->
+  sem_type G (TFun T1 T2) (tabs t).
+Proof.
+  unfold sem_type; simpl; intros; eapply vtp_etp with (nm := 0).
+  - unfoldTeval; intros; step_eval; trivial.
+  - unfold etp in *; simp vtp; eauto.
+Qed.
+
+Lemma fund_t_var: forall G x T, indexr x G = Some T -> sem_type G T (tvar x).
+Proof.
+  unfold sem_type, etp, expr_sem; intros;
+  (* repeat (intros; hnf). *)
+  pose proof (teval_var env x); eval_det; subst.
+  edestruct R_env_to_indexr_success; eauto.
+Qed.
+
+(** Fundamental property, application case.
+ **** Proof sketch.
+      It's easy to show that the result has the right type, *if it exists*, but
+      showing it exists is harder.
+
+      That is, what's harder is showing that if evaluation of the application [t
+      = tapp t1 t2] terminates without timeout, then it does not fail. That's
+      because the various proof steps are closely interlocked.
+
+      Evaluation of [t]  proceeds by evaluating [t2], then [t1], and then
+      performing application, in the evaluation monad; failures (timeouts and
+      runtime errors) interrupt evaluation and propagate.
+      If [t] does not timeout, then [t2] does not time out (by case analysis);
+      then, since [t2] is semantically well-typed, its evaluation does not fail.
+      Then, evaluation [t] proceeds to [t1], which by the same reasoning neither
+      times out nor fails; moreover, it produces a closure.
+      Finally, evaluating [t] proceeds to applying the closure, which by the
+      same reasoning neither times out nor fails, producing a well-typed result. *)
+Lemma fund_t_app: forall G T1 T2 t1 t2, sem_type G (TFun T1 T2) t1 -> sem_type G T1 t2 -> sem_type G T2 (tapp t1 t2).
+Proof.
+  unfold sem_type, etp, expr_sem; unfoldTeval;
+  intros * Hfun Harg ? ? * [nmR HappEv].
+
+  (* Various implementations of the same case analysis are possible.
+     It's faster to only do as much case analysis as strictly needed. *)
+
+
+  (* V1 Fast *)
+  n_is_succ_hp;
+    (** We must show that nmR is at least one, since that's required by the
+        hypothesis of semantic expression typing for Hfun and Harg. *)
+    destruct nmR;
+    (* The iteration counts are optimized for speed, but it's also OK to do all *)
+    (*   case splits in advance as in V1.1. *)
+    do 2 better_case_match_ex; edestruct Harg; ev; eauto; try discriminate; injectHyps;
+      do 3 better_case_match_ex; edestruct Hfun; ev; eauto; try discriminate; injectHyps;
+        repeat better_case_match_ex; simp vtp in *; unfold expr_sem in *; unfoldTeval; eauto; contradiction.
+
+  (* V1.1 less fast, more maintainable. *)
+  (* n_is_succ_hp; destruct nmR; *)
+  (*   repeat better_case_match_ex; edestruct Harg; ev; eauto; try discriminate; injectHyps; *)
+  (*     better_case_match_ex; edestruct Hfun; ev; eauto; try discriminate; injectHyps; *)
+  (*       simp vtp in *; unfold expr_sem in *; unfoldTeval; eauto; contradiction. *)
+Qed.
+
+Lemma fund_t_nat: forall G n,
+  sem_type G TNat (tnat n).
+Proof.
+  unfold sem_type; intros; eapply vtp_etp with (nm := 0).
+  - unfoldTeval; intros; step_eval; trivial.
+  - unfold etp in *; simp vtp; eauto.
+Qed.
+
+(** Fundamental property.
+    Proved by induction on typing derivations. *)
+Theorem fundamental: forall G t T, has_type G t T -> sem_type G T t.
+Proof. intros * Htp; induction Htp; eauto using fund_t_var, fund_t_nat, fund_t_abs, fund_t_app. Qed.
+
+(** Type soundness: If evaluation of a well-typed program terminates, the result
+    is not a runtime error. Proved as a corollary of the [fundamental] property. *)
+Theorem sound: forall G t T env optV j, has_type G t T ->
+    R_env env G ->
+    tevalSnOpt env t optV j ->
+    exists v, optV = Some v /\ vtp T v.
+Proof. intros; edestruct fundamental; ev; eauto. Qed.
+(* Concluding tevalSn env t v j -> vtp T v would talk only about successful
+   evaluations, not about runtime errors! *)
