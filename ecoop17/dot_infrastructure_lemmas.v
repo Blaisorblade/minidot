f32324fd7ed06288d4351c6f6dcfd5c7797401b7
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2018-10-15 20:36:46 +0200

    Join copy-pasted lemmas on indexr

--- ecoop17/dot_infrastructure_lemmas.v
+++ ecoop17/dot_infrastructure_lemmas.v
@@ -37,3 +37,36 @@ Proof.
     assert (beq_nat n (length vs) = false) as -> by eauto with eq;
     reflexivity.
 Qed.
+
+Lemma indexr_succ_wk: forall {X} i (G G': list X) r, indexr i G = Some r -> indexr (i + length G') (G ++ G') = Some r.
+Proof.
+  intros; induction G; simpl in *; try discriminate;
+  rewrite app_length; repeat better_case_match_ex; eauto; omega.
+Qed.
+
+(** Simplifies next proof. *)
+Lemma indexr_fail_eq: forall {X} i (G: list X), (indexr i G = None) <-> (i >= length G).
+Proof.
+  intros.
+  split; intros; gen i; induction G; simpl; intros; eauto.
+  - better_case_match_ex; assert (i >= length G) by eauto; omega.
+  - better_case_match_ex; eauto 2; omega.
+Qed.
+
+Lemma indexr_fail_wk: forall {X} i (G G': list X), indexr i G = None -> indexr (i + length G') (G ++ G') = None.
+Proof.
+  (* Really manual proof: *)
+  intros * H.
+  (** Translate the problem to an inequality on lengths. *)
+  eapply indexr_fail_eq; eapply indexr_fail_eq in H.
+  (** Then solve it. *)
+  rewrite app_length; omega.
+Qed.
+
+Lemma indexr_wk: forall {X} i (G G': list X) r, indexr i G = r -> indexr (i + length G') (G ++ G') = r.
+  destruct r; eauto using indexr_succ_wk, indexr_fail_wk. Qed.
+Hint Resolve indexr_wk.
+
+Lemma indexr_wk_eq: forall {X} i (G G': list X), indexr i G = indexr (i + length G') (G ++ G').
+  intros; erewrite indexr_wk; eauto.
+Qed.
--- ecoop17/stlc.v
+++ ecoop17/stlc.v
@@ -82,44 +82,10 @@ Definition rename (sigma: id -> id): tm -> tm :=
     end.
 Definition wk n' := rename (fun n => n + n').
 
-Lemma indexr_succ_wk: forall {X} i (G G': list X) r, indexr i G = Some r -> indexr (i + length G') (G ++ G') = Some r.
-Proof.
-  intros; induction G; simpl in *; try discriminate;
-  rewrite app_length; repeat better_case_match_ex; eauto; omega.
-Qed.
-
 (* Require Import Setoid. *)
 (* (* Stolen from https://github.com/coq/coq/issues/3814, and dangerous, but enable setoid_rewrite using equalities on the goal. *) *)
 (* Instance subrelation_eq_impl : subrelation eq impl. congruence. Qed. *)
 (* Instance subrelation_eq_flip_impl : subrelation eq (flip impl). congruence. Qed. *)
-
-(** Simplifies next proof. *)
-Lemma indexr_fail_eq: forall {X} i (G: list X), (indexr i G = None) <-> (i >= length G).
-Proof.
-  intros.
-  split; intros; gen i; induction G; simpl; intros; eauto.
-  - better_case_match_ex; assert (i >= length G) by eauto; omega.
-  - better_case_match_ex; eauto 2; omega.
-Qed.
-
-(* Unused *)
-Lemma indexr_fail_wk: forall {X} i (G G': list X), indexr i G = None -> indexr (i + length G') (G ++ G') = None.
-Proof.
-  (* Really manual proof: *)
-  intros * H.
-  (** Translate the problem to an inequality on lengths. *)
-  eapply indexr_fail_eq; eapply indexr_fail_eq in H.
-  (** Then solve it. *)
-  rewrite app_length; omega.
-Qed.
-
-Lemma indexr_wk: forall {X} i (G G': list X) r, indexr i G = r -> indexr (i + length G') (G ++ G') = r.
-  destruct r; eauto using indexr_succ_wk, indexr_fail_wk. Qed.
-Hint Resolve indexr_wk.
-
-Lemma indexr_wk_eq: forall {X} i (G G': list X), indexr i G = indexr (i + length G') (G ++ G').
-  intros; erewrite indexr_wk; eauto.
-Qed.
 
 Lemma wk_has_type: forall t b G G' T, has_type b G t T -> has_type b (G ++ G') (wk (length G') t) T.
 Proof.
